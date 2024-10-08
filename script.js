e0d5fb60ee1e6293f526575476102a1aee89b646
sonu sharma <kumar703994@gmail.com>
2024-10-08 15:07:49 +0530

    Added File

---
+++ script.js
@@ -0,0 +1,43 @@
+let htmlinput = document.querySelector(".html-editor textarea");
+        let cssinput = document.querySelector(".css-editor textarea");
+        let jsinput = document.querySelector(".js-editor textarea");
+        let save = document.querySelector("#save");
+        let output = document.querySelector("#output");
+        let outputContainer = document.querySelector(".output-container");
+        let full = document.querySelector("#full");
+        let copy = document.querySelectorAll(".copy");
+
+        save.addEventListener("click", () => {
+            // Setting HTML content
+            output.contentDocument.body.innerHTML = htmlinput.value;
+
+            // Setting CSS content
+            output.contentDocument.head.innerHTML = `<style>${cssinput.value}</style>`;
+
+            // Evaluating JavaScript code
+            output.contentWindow.eval(jsinput.value);
+        });
+
+        full.addEventListener("click",() =>{
+            outputContainer.classList.toggle("output-full-active")
+            // console.log("HELLO")
+            if(outputContainer.classList.contains("output-full-active")){
+                full.style.transform="rotate(180deg)"
+            }else{
+                full.style.transform="rotate(0deg)"
+            }
+        })
+
+        copy.forEach((e)=>{
+            e.addEventListener("click",()=>{
+                if(e.classList.contains("copy1")){
+                    navigator.clipboard.writeText(htmlinput.value)
+                }
+                else if(e.classList.contains("copy2")){
+                    navigator.clipboard.writeText(cssinput.value)
+                }else{
+                    navigator.clipboard.writeText(jsinput.value)
+                }
+            })
+            
+        });
